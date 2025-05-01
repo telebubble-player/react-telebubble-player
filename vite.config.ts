97e390a89cf943405a50246253fde5f433ff6e3a
Sanny <sanny.sawwent@icloud.com>
2025-05-01 21:52:36 +0300

    fix: buld

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // or your entry file
+      name: 'ReactTelebubblePlayer',
+      fileName: (format) => `react-telebubble-player.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
